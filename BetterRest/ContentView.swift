90a6d9d4d4d4537b386da63776cc0c2aeb9a259d
ÄTAGWE ROGER <atagweroger2@gmail.com>
2026-07-03 09:19:50 +0100

    Built a basic layout for my app and add my ml model

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,15 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
+    @State private var sleepAmt = 8.0
+    @State private var coffeAmt = 1
     var body: some View {
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-            .labelsHidden()
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up")
+                    .font(.headline)
+                DatePicker("Please enter time" , selection: $wakeUp , displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmt.formatted()) hours" , value: $sleepAmt, in: 4...12 , step: 0.25)
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper("\(coffeAmt) cup(s)", value: $coffeAmt, in: 1...20)
+            }
+            .padding(20)
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
+        }
+        
        
     }
-    func exampleDate () {
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow
+    func calculateBedTime() {
+        
     }
+   
 }
 
 #Preview {
